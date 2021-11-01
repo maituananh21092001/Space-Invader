a3a38874575b2ee0e1d71c631676e9308ec2348d
giang428 <giangcua3d@gmail.com>
2021-11-01 15:27:56 +0700

    Add highscore function from dev branch

--- object/gameover.py
+++ object/gameover.py
@@ -1,4 +1,5 @@
 import pygame
+from os import path
 from object.const import *
 def gameover(self):
                 newGame = False
@@ -21,6 +22,14 @@ def gameover(self):
                     self.text(self.xScreen/2 - 100, self.yScreen - 100, "RETRY", 50,'./data/font/ARCADE_R.TTF',GREEN)  # In điểm
                     self.text(self.xScreen/2-100, self.yScreen/2-100,
                                     "GAME OVER", 50,'./data/font/ARCADE_I.TTF',WHITE)  # In Thông báo thua
+                    self.draw_text("Press a key to play again", 22, WHITE, WIDTH / 2, HEIGHT * 3 / 4)
+                    if self.scores > self.highscore:
+                            self.highscore = self.scores
+                            self.draw_text("NEW HIGH SCORE!", 22, WHITE, WIDTH / 2, HEIGHT / 2 + 20)
+                            with open(path.join(self.dir, HS_FILE), 'w') as f:
+                                f.write(str(self.scores))
+                    else:
+                        self.draw_text("High Score: " + str(self.highscore), 22, WHITE, WIDTH / 2, HEIGHT / 2 + 40)
                     pygame.display.update()
                 self.scores = 0      # Trả các biến về giá trị ban đầu
                 self.listBullet = []
