0efc4254b1985588761963325b7e55bf4793b134
giang428 <giangcua3d@gmail.com>
2021-11-01 15:21:51 +0700

    Import const.py to files

--- object/game.py
+++ object/game.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 from pygame import mixer
+from object.const import *
 from pygame.constants import MOUSEBUTTONDOWN, MOUSEBUTTONUP, MOUSEMOTION
 from object.gameover import gameover
 import sys
@@ -202,33 +203,3 @@ class Game:
             self.image_draw(self.linkPlanes, self.xPlanes,
                             self.yPlanes, self.sizexPlanes, self.sizeyPlanes)
             pygame.display.update()  # Update
-# game options/settings
-TITLE = "Jumpy!"
-WIDTH = 800
-HEIGHT = 600
-FPS = 60
-FONT_NAME = 'arial'
-HS_FILE = "highscores.txt"
-
-# Player properties
-PLAYER_ACC = 0.5
-PLAYER_FRICTION = -0.12
-PLAYER_GRAV = 0.8
-PLAYER_JUMP = 20
-
-# Starting platforms
-PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
-                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20),
-                 (125, HEIGHT - 350, 100, 20),
-                 (350, 200, 100, 20),
-                 (175, 100, 50, 20)]
-
-# define colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-LIGHTBLUE = (0, 155, 155)
-BGCOLOR = LIGHTBLUE
--- object/gameover.py
+++ object/gameover.py
@@ -1,11 +1,5 @@
 import pygame
-WIDTH = 1280
-HEIGHT = 576
-BLACK = (0,0,0)
-WHITE = (255,255,255)
-RED = (255,0,0)
-BLUE = (0,255,0)
-GREEN = (0,0,255)
+from object.const import *
 def gameover(self):
                 newGame = False
                 self.screen.fill(BLACK)
